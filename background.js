b7c6d02739ac797f7ef2f4475b48807a5bf13504
Izaac Tay <izaactay1@gmail.com>
2023-05-18 01:03:44 -0700

    Added calls to reviews API

--- background.js
+++ background.js
@@ -1,13 +1,34 @@
+var curr_place_id = '';
+
 
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
     if (tab.url && tab.url.includes("google.com/maps/place")) {
-        const queryParameters = tab.url.split("/")[5];
-        console.log(queryParameters);
-        console.log("logged!");
 
-        chrome.tabs.sendMessage(tabId, {
-            type: "NEW",
-            Name: queryParameters
-        });
+        const queryParameters = tab.url.split("/");
+        var place_name = queryParameters[5];
+        var data = queryParameters[7];
+        var place_id = data.split('5!1s')[1].split('!8m2!')[0];
+
+        
+        console.log(place_id);
+        if (place_id != curr_place_id){
+
+            update(place_id,tabId,place_name);
+            curr_place_id = place_id;
+        }
     }
 });
+
+async function update(place_id,tabId,place_name) {
+    let url = "https://touristy-extension.herokuapp.com/";
+    url = url.concat(place_id);
+    const response  = await fetch(url);
+    const jsonData = await response.json();
+    console.log(jsonData);
+    chrome.tabs.sendMessage(tabId, {
+        type: "NEW",
+        Name: place_name,
+        Score: jsonData
+    });
+    console.log('message sent')
+}
--- contentScript.js
+++ contentScript.js
@@ -2,15 +2,17 @@
     console.log("i am reading google maps!");
     
     chrome.runtime.onMessage.addListener((obj,sender,response) => {
-        const type = obj;
+        
 
-        console.log(type);
+
 
         var div = document.createElement("rating_div"); 
         //console.log(document.getElementsByClassName("F7nice mmu3tf"));
         //console.log(document.getElementsByClassName("F7nice mmu3tf").length);
         document.getElementsByClassName("lMbq3e")[0].appendChild(div);
-        div.innerText="Test123"
+        let score_text = "Percentage of local reviews:"
+        score_text = score_text.concat(obj.Score)
+        div.innerText=score_text
 
 
 
